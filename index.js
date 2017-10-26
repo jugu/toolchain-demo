009a85753416e7d37d01aa44347cf4ecbf2116c3
Jugu Dannie Sundar <Jugu.Dannie.Sundar@ibm.com>
2017-10-26 13:49:10 -0700

    updated demo:

--- index.js
+++ index.js
@@ -1,14 +1,42 @@
 const
     express = require('express'),
-    nconf = require("nconf");
+    nconf = require("nconf"),
+    i18n = require("i18n");
 const app = express();
 
+i18n.configure({
+    locales:['en', 'de'],
+    directory: __dirname + '/locales',
+    prefix: 'resources_'
+});
 app.set('view engine', 'pug');
 
+app.use(i18n.init);
 /* GET home page. */
 app.get('/', function (req, res) {
-  res.render('index', { title: 'Hey', message: 'Hello there!' })
+  loadData(req, res, "en")
 })
+app.get('/de', function(req, res){
+  loadData(req, res, "de")
+});
+app.get('/es', function(req, res){
+  loadData(req, res, "es")
+});
+app.get('/ja', function(req, res){
+  loadData(req, res, "ja")
+});
+
+function loadData(req, res, locale) {
+  i18n.setLocale(locale);
+  var data =  {
+    selectlocale: "select a locale",
+    locales: ["en", "de"],
+    title: 'Translation Demo',
+    message: i18n.__('Hello'),
+    kvlist: [i18n.__('A'), i18n.__('B'), i18n.__('C')]
+  }
+  res.render('index', data);
+}
 
 function populateNconfSync() {
 	/* Load up configuration.
@@ -27,6 +55,7 @@ function populateNconfSync() {
 	if (process.env.VCAP_APPLICATION)
 		overrides._vcap_application = JSON.parse(process.env.VCAP_APPLICATION);
 
+  nconf.file("default", "config/dev.json");
 	if (Object.getOwnPropertyNames(nconf.overrides).length !== 0) {
 		nconf.overrides(overrides);
 	}
@@ -35,5 +64,5 @@ function populateNconfSync() {
 populateNconfSync();
 
 app.listen(nconf.get("PORT"), function () {
-  console.log('Example app listening on port 3000!')
+  console.log('Example app listening on port ', nconf.get("PORT"))
 })
